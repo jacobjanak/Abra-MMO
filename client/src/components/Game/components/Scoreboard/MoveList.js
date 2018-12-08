762541d7bd7a5e943dcc29127af0712b965a9f44
Jacob Janak <jakeathotmail@gmail.com>
2018-12-08 14:44:39 -0800

    Attempted to add in autoscroll which required me to update to react 16. Everything seems to work with React 16

---
+++ client/src/components/Game/components/Scoreboard/MoveList.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import autoscroll from 'autoscroll-react';
+
+class MoveList extends Component {
+    formatMove = move => {
+        const xy = move.split(',');
+        let formatted = '(';
+
+        for (let i = 0; i < xy.length; i++) {
+            if (xy[i].length === 1) {
+                formatted += ' ' + xy[i] + ' ';
+            }
+            else if (xy[i].length === 2) {
+                formatted += ' ' + xy[i];
+            }
+            else if (xy[i].length >= 3) {
+                formatted += xy[i];
+            }
+            if (i === 0) formatted += ',';
+        }
+
+        formatted += ')';
+
+        return formatted;
+    }
+
+    render() {
+        const { moves, ...props } = this.props;
+
+        const moveListContent = [];
+        for (let i = 0; i < moves.length; i += 2) {
+            console.log(i)
+            moveListContent.push(() => (
+                <div className="move-row">
+                    <span className="move-number">{Math.floor(i / 2) + 1}.</span>
+                    <span className="move">{ this.formatMove(moves[i]) }</span>
+                    <span className="move">
+                        {
+                          moves[i + 1]
+                          ? this.formatMove(moves[i + 1])
+                          : <span style={{ color: 'transparent' }}>{ this.formatMove('0,0') }</span>
+                        }
+                    </span>
+                </div>
+            ))
+        }
+
+        return (
+            <div id="move-list-container" { ...props }>
+                <div id="move-list">
+                    { moveListContent.map((Move, i) => (
+                        <Move key={i} />
+                    ))}
+                </div>
+            </div>
+        );
+    }
+}
+ 
+export default autoscroll(MoveList);
--- client/src/components/Game/components/Scoreboard/Scoreboard.js
+++ client/src/components/Game/components/Scoreboard/Scoreboard.js
@@ -1,59 +1,20 @@
 import React, { Component } from 'react';
+import MoveList from './MoveList';
 import './Scoreboard.css';
 
 class Scoreboard extends Component {
-    state = {
-
-    };
-
-    formatMove = move => {
-        const xy = move.split(',');
-        let formatted = '(';
-
-        for (let i = 0; i < xy.length; i++) {
-            if (xy[i].length === 1) {
-                formatted += ' ' + xy[i] + ' ';
-            }
-            else if (xy[i].length === 2) {
-                formatted += ' ' + xy[i];
-            }
-            else if (xy[i].length >= 3) {
-                formatted += xy[i];
-            }
-            if (i === 0) formatted += ',';
-        }
-
-        formatted += ')';
-
-        return formatted;
-    }
-
     render() {
         const { player1, player2, moves, winner }= this.props;
-
-        const moveListContent = [];
-        for (let i = 0; i < moves.length; i += 2) {
-            console.log(i)
-            moveListContent.push(() => (
-                <div className="move-row">
-                    <span className="move-number">{Math.floor(i / 2) + 1}.</span>
-                    <span className="move">{ this.formatMove(moves[i]) }</span>
-                    <span className="move">
-                        {
-                          moves[i + 1]
-                          ? this.formatMove(moves[i + 1])
-                          : <span style={{ color: 'white' }}>{ this.formatMove('0,0') }</span>
-                        }
-                    </span>
-                </div>
-            ))
-        }
 
         return (
             <div id="scoreboard">
                 <p className="lead">
                     {player1.username}
                     <span className="timer">3:43</span>
+                </p>
+                <p className="lead">
+                    {player2.username}
+                    <span className="timer">5:07</span>
                 </p>
                 <div id="move-controls">
                     <span className="move-control">
@@ -69,17 +30,9 @@ class Scoreboard extends Component {
                         <i className="fas fa-fast-forward"></i>
                     </span>
                 </div>
-                <div id="move-list-container">
-                    <div id="move-list">
-                        { moveListContent.map((Move, i) => (
-                            <Move key={i} />
-                        ))}
-                    </div>
-                </div>
-                <p className="lead">
-                    {player2.username}
-                    <span className="timer">5:07</span>
-                </p>
+
+                <MoveList moves={moves} />
+
                 {/* <p style={{ color: 'red' }}>
                     Player 1: {player1.username}&nbsp;
                     {moves.length % 2 === 0 && !winner && (

c7e59f1a919da1b12efa447234fcffd17322f776
Jacob Janak <jjanak@paytronix.com>
2024-04-21 19:03:48 -0400

    Don't open socket until I actually need it

--- client/src/API.js
+++ client/src/API.js
@@ -2,8 +2,15 @@ import axios from 'axios';
 import openSocket from 'socket.io-client';
 import AuthService from './components/AuthService';
 
-const socket = openSocket(window.location.origin);
 const Auth = new AuthService();
+
+function socket() {
+    if (socket.instance)
+        return socket.instance;
+
+    socket.instance = openSocket(window.location.origin);
+    return socket.instance;
+}
 
 const API = {
     getUser: (username = '') => {
@@ -20,30 +27,30 @@ const API = {
     },
     queue: cb => {
         const userId = Auth.user().id;
-        socket.emit('queue', userId)
+        socket().emit('queue', userId)
         API.openSocket(cb)
     },
     dequeue: () => {
-        socket.emit('dequeue')
+        socket().emit('dequeue')
     },
     joinGame: (gameId, cb) => {
         API.openSocket(cb)
         const userId = Auth.user().id;
-        socket.emit('joinGame', {
+        socket().emit('joinGame', {
             gameId,
             userId
         })
     },
     openSocket: cb => {
-        socket.on('gameJoined', game => cb(game, null, null))
-        socket.on('newMove', move => cb(null, move, null))
-        socket.on('winner', winner => cb(null, null, winner))
+        socket().on('gameJoined', game => cb(game, null, null))
+        socket().on('newMove', move => cb(null, move, null))
+        socket().on('winner', winner => cb(null, null, winner))
     },
     getPlayerCount: cb => {
-        socket.emit('getPlayerCount')
-        socket.on('playerCount', playerCount => cb(playerCount))
+        socket().emit('getPlayerCount')
+        socket().on('playerCount', playerCount => cb(playerCount))
     },
-    move: move => socket.emit('move', move)
+    move: move => socket().emit('move', move)
 };
 
 export default API;

7a7110f91a9dcaec30a757b37154b62e09c25e4f
Jacob Janak <jjanak@paytronix.com>
2024-05-05 17:49:13 -0400

    Improve navbar, especially the highlighting

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Link} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import AuthService from '../AuthService';
 import './NavBar.css';
 
@@ -20,57 +20,65 @@ class NavBar extends Component {
     };
 
     render() {
-        const {username} = this.state;
-        const path = window.location.pathname;
+        const { username } = this.state;
 
         return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark" style={{zIndex: 500}}>
-                <Link className="navbar-brand mr-4 ml-2" to="/">Abra</Link>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <NavLink className="navbar-brand mr-4 ml-2" to="/">
+                    Abra
+                </NavLink>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbar"
+                >
                     <span className="navbar-toggler-icon"/>
                 </button>
                 <div className="collapse navbar-collapse" id="navbar">
                     <ul className="navbar-nav">
                         <li className="nav-item mr-3">
-                            <Link
-                                className={"nav-item nav-link" + (path === "/play" ? " active" : "")}
-                                to={"/play"}>
+                            <NavLink className="nav-item nav-link" to="/play">
                                 Play Offline
-                            </Link>
+                            </NavLink>
                         </li>
                         <li className="nav-item mr-3">
-                            <a
-                                className={"nav-item nav-link" + (path === "/online" ? " active" : "")}
-                                href="/online">
+                            <NavLink className="nav-item nav-link" to="/online">
                                 Online
-                            </a>
+                            </NavLink>
                         </li>
                     </ul>
                     {username ? (
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown"
-                                   data-toggle="dropdown">
+                                <a className="nav-link dropdown-toggle" href="#" data-toggle="dropdown">
                                     {username}
                                 </a>
                                 <div className="dropdown-menu dropdown-menu-right mt-3">
-                                    <Link className="dropdown-item" to={"/profile/" + username}>Profile</Link>
+                                    <NavLink className="dropdown-item" to={"/profile/" + username}>
+                                        Profile
+                                    </NavLink>
                                     <div className="dropdown-divider"></div>
-                                    <a className="dropdown-item" href="#" onClick={this.handleLogout}>Logout</a>
+                                    <a className="dropdown-item" href="#" onClick={this.handleLogout}>
+                                        Logout
+                                    </a>
                                 </div>
                             </li>
                         </ul>
                     ) : (
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item">
-                                <Link className="btn btn-outline-light mr-2" to={"/signup"}>Sign up</Link>
+                                <NavLink className="btn btn-outline-light mr-2" to="/signup">
+                                    Sign up
+                                </NavLink>
                             </li>
                             <li className="nav-item">
-                                <Link className="btn btn-dark" to={"/login"}>Login</Link>
+                                <NavLink className="btn btn-dark" to="/login">
+                                    Login
+                                </NavLink>
                             </li>
                         </ul>
                     )}
-
                 </div>
             </nav>
         );

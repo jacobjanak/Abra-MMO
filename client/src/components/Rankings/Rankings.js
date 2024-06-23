f1ca11082723decb5567729ddc9b66d9e2ea2d62
Jacob Janak <jjanak@paytronix.com>
2024-06-23 14:44:55 -0400

    Improve users page

--- client/src/components/Rankings/Rankings.js
+++ client/src/components/Rankings/Rankings.js
@@ -31,7 +31,7 @@ class Rankings extends Component {
 
         return (
             <div>
-                <h4 id="search-header">Search User</h4>
+                <h4 id="search-header">User Search</h4>
                 <div id="search-container" className="input-group">
                     <input type="text" className="form-control" onChange={this.searchChange} />
                     <button className="btn btn-primary" type="button" onClick={this.handleSearch}>
@@ -39,41 +39,45 @@ class Rankings extends Component {
                     </button>
                 </div>
 
-                <h4 id="rankings-header">
-                    <img className="trophy" src="../trophy.svg" alt="trophy" />
-                    Leaderboard
-                    <img className="trophy" src="../trophy.svg" alt="trophy" />
-                </h4>
-                <div id="table-container">
-                    <table>
-                        <thead>
-                        <tr>
-                            <th></th>
-                            <th className="username">
-                                Player
-                            </th>
-                            <th>Rating</th>
-                            <th>Won</th>
-                            <th>Lost</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        {users.map((user, i) => (
-                            <tr key={user._id}>
-                                <td>{i + 1}.</td>
-                                <td className="username">
-                                    <a href={"/profile/" + user.username}>
-                                        {user.username}
-                                    </a>
-                                </td>
-                                <td>{user.rating}</td>
-                                <td>{user.wins}</td>
-                                <td>{user.losses}</td>
-                            </tr>
-                        ))}
-                        </tbody>
-                    </table>
-                </div>
+                { users.length && (
+                    <div>
+                        <h4 id="rankings-header">
+                            <img className="trophy" src="../trophy.svg" alt="trophy" />
+                            Leaderboard
+                            <img className="trophy" src="../trophy.svg" alt="trophy" />
+                        </h4>
+                        <div id="table-container">
+                            <table>
+                                <thead>
+                                <tr>
+                                    <th></th>
+                                    <th className="username">
+                                        Player
+                                    </th>
+                                    <th>Rating</th>
+                                    <th>Won</th>
+                                    <th>Lost</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                {users.map((user, i) => (
+                                    <tr key={user._id}>
+                                        <td>{i + 1}.</td>
+                                        <td className="username">
+                                            <a href={"/profile/" + user.username}>
+                                                {user.username}
+                                            </a>
+                                        </td>
+                                        <td>{user.rating}</td>
+                                        <td>{user.wins}</td>
+                                        <td>{user.losses}</td>
+                                    </tr>
+                                ))}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                )}
             </div>
         );
     }
